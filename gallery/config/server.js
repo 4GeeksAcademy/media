9f3bba8cb8b636ce991d226b248c49d8ba8ad48d
Alejandro Sanchez <aalejo@gmail.com>
2020-12-28 12:07:41 -0500

    initial commit

---
+++ gallery/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5dafd3067b1b1897621535165e8bca47'),
+    },
+  },
+});
---
+++ gallery/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '98a10cca-e563-4a8e-863a-7404c493965d'
+};
